b003f95e0fb03207c7902abcefd7b117cab9a186
Gustavo Goldhardt <gustavo.goldhardt@gmail.com>
2022-06-20 20:46:19 -0300

    Removed unused query

--- core/src/main/java/tech/gusgol/core/db/WorkoutDao.kt
+++ core/src/main/java/tech/gusgol/core/db/WorkoutDao.kt
@@ -12,12 +12,11 @@ interface WorkoutDao {
     @Query("SELECT * FROM workout")
     fun getAll(): List<Workout>
 
+    @Query("SELECT * FROM workout")
+    fun getAllStream(): Flow<List<Workout>>
+
     @Query("SELECT * FROM workout WHERE uid = :workoutId")
     fun getWorkout(workoutId: String): Flow<Workout?>
-
-//    @Query(
-//        "SELECT * FROM workout JOIN exercise ON workout.uid = exercise.workout_id WHERE workout.uid = :workoutId")
-//    fun getWorkoutWithExercises(workoutId: String): Flow<Map<Workout, List<Exercise>>>
 
     @Transaction
     @Query("SELECT * FROM workout WHERE uid = :workoutId")
