f0e332b956d943e718d8d092aeaaafa0e256c7f5
Gustavo Goldhardt <gustavo.goldhardt@gmail.com>
2022-07-06 19:49:36 -0300

    Moved all Workout nav composables to their own graph

--- app/src/main/java/tech/gusgol/portabletrainer/navigation/NavGraph.kt
+++ app/src/main/java/tech/gusgol/portabletrainer/navigation/NavGraph.kt
@@ -3,15 +3,9 @@ package tech.gusgol.portabletrainer.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import tech.gusgol.portabletrainer.ui.workouts.list.active.ActiveWorkoutsRoute
-import tech.gusgol.portabletrainer.ui.workouts.list.archived.ArchivedWorkoutsRoute
-import tech.gusgol.portabletrainer.ui.workouts.create.CreateWorkoutRoute
 import tech.gusgol.portabletrainer.ui.workouts.navigation.ActiveWorkoutsDestination
-import tech.gusgol.portabletrainer.ui.workouts.navigation.ArchivedWorkoutsWorkoutDestination
-import tech.gusgol.portabletrainer.ui.workouts.navigation.CreateWorkoutDestination
-import tech.gusgol.portabletrainer.ui.workouts.navigation.workoutDetailGraph
+import tech.gusgol.portabletrainer.ui.workouts.navigation.workoutsGraph
 
 @Composable
 fun PortableTrainerNavGraph(
@@ -22,15 +16,6 @@ fun PortableTrainerNavGraph(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(ActiveWorkoutsDestination.route) {
-            ActiveWorkoutsRoute(navController)
-        }
-        composable(ArchivedWorkoutsWorkoutDestination.route) {
-            ArchivedWorkoutsRoute(navController)
-        }
-        composable(CreateWorkoutDestination.route) {
-            CreateWorkoutRoute(navController)
-        }
-        workoutDetailGraph { navController.popBackStack() }
+        workoutsGraph(navController)
     }
 }
--- app/src/main/java/tech/gusgol/portabletrainer/ui/workouts/navigation/WorkoutsNavigation.kt
+++ app/src/main/java/tech/gusgol/portabletrainer/ui/workouts/navigation/WorkoutsNavigation.kt
@@ -1,33 +1,47 @@
 package tech.gusgol.portabletrainer.ui.workouts.navigation
 
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import tech.gusgol.portabletrainer.navigation.NavigationDestination
+import tech.gusgol.portabletrainer.ui.workouts.create.CreateWorkoutRoute
 import tech.gusgol.portabletrainer.ui.workouts.detail.WorkoutDetailRoute
+import tech.gusgol.portabletrainer.ui.workouts.list.active.ActiveWorkoutsRoute
+import tech.gusgol.portabletrainer.ui.workouts.list.archived.ArchivedWorkoutsRoute
 
 const val WORKOUT = "workout"
+const val WORKOUTS = "workouts"
 
 object CreateWorkoutDestination: NavigationDestination {
-    override val route: String = "workout/create"
+    override val route: String = "$WORKOUT/create"
 }
 
 object ActiveWorkoutsDestination: NavigationDestination {
-    override val route: String = "workouts/active"
+    override val route: String = "$WORKOUTS/active"
 }
 object ArchivedWorkoutsWorkoutDestination: NavigationDestination {
-    override val route: String = "workouts/archived"
+    override val route: String = "$WORKOUTS/archived"
 }
 
 object WorkoutDetailDestination : NavigationDestination {
-    override val route: String = "workout/detail"
+    override val route: String = "$WORKOUT/detail"
     const val workoutIdArg = "workoutId"
 }
 
-fun NavGraphBuilder.workoutDetailGraph(
-    onBackClick: () -> Unit
+fun NavGraphBuilder.workoutsGraph(
+    navController: NavController
 ) {
+    composable(ActiveWorkoutsDestination.route) {
+        ActiveWorkoutsRoute(navController)
+    }
+    composable(ArchivedWorkoutsWorkoutDestination.route) {
+        ArchivedWorkoutsRoute(navController)
+    }
+    composable(CreateWorkoutDestination.route) {
+        CreateWorkoutRoute(navController)
+    }
     composable(
         route = "${WorkoutDetailDestination.route}/{${WorkoutDetailDestination.workoutIdArg}}",
         arguments = listOf(
@@ -36,6 +50,10 @@ fun NavGraphBuilder.workoutDetailGraph(
             }
         )
     ) {
-        WorkoutDetailRoute(onBackClick)
+        WorkoutDetailRoute(
+            onBackClick = {
+                navController.popBackStack()
+            }
+        )
     }
 }
